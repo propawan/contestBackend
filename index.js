9d15ba7752fc0fad89bd38d81aed4939448a9b3f
Pawan Kumar <pawan.kumar@betterplace.co.in>
2022-05-20 19:20:45 +0530

    Add boilerplate code.

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`server is listening on port ${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
