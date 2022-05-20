623a7438263173767113d35774da4e25457eba35
Pawan Kumar <pawankumarpawankumar619@gmail.com>
2022-05-21 01:26:05 +0530

    Refactor Connect with DB code.

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 require("dotenv").config();
 require("express-async-errors");
 
+const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
-const connectDB = require("./db/connect");
 
 app.use(express.json());
 
@@ -11,7 +11,7 @@ const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
-    await connectDB(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI);
     app.listen(port, console.log(`server is listening on port ${port}...`));
   } catch (error) {
     console.log(error);
