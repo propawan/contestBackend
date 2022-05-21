c99c2ebf419506a466fc861b5e896661a866b2e9
Vamsi_Krishna_027 <vamsi@oustlabs.com>
2022-05-21 21:27:28 +0530

    score api created

--- index.js
+++ index.js
@@ -5,9 +5,11 @@ const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
 const users = require("./routes/users");
+const score = require("./routes/score");
 
 app.use(express.json());
 app.use("/api/v1/users", users);
+app.use("/api/v1/score", score);
 
 const port = process.env.PORT || 3000;
 
