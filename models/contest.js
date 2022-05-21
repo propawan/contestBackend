94c33b1b8999e0041e5ebeb80328d32c8ae909a4
Tushar Agrawal <tushar.agarwal@unthinkable.co>
2022-05-21 20:19:09 +0530

    Add Contest Schema code

---
+++ models/contest.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+const TaskSchema = new mongoose.Schema({
+  contestName: {
+    type: String,
+    required: [true, "must provide Contest Name"],
+    trim: true,
+    maxlength: [50, "Contest Name can't be more than 50 chars long"],
+  },
+  companyName: {
+    type: String,
+    required: [true, "must provide Company Name"],
+    trim: true,
+    maxlength: [30, "Company Name can't be more than 30 chars long"],
+  },
+  requiredExperience: {
+    type: Number,
+    required: [true, "must provide Required Experience"],
+  },
+  totalQuestion: {
+    type: Number,
+    required: [true, "must provide Total Question"],
+  },
+  profileDescription: {
+    type: String,
+    required: [true, "must provide Profile Description"],
+    trim: true,
+    maxlength: [200, "Profile Description can't be more than 200 chars long"],
+  },
+  contestDataAndTime: {
+    type: Date,
+    required: [true, "must provide Contest Data And Time"],
+  },
+});
+
+module.exports = mongoose.model("Contest", ContestSchema);
