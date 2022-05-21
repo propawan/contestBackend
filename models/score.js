13f38368f8027cbeb62a707ed30d95a9176c7c1d
Vamsi_Krishna_027 <vamsi@oustlabs.com>
2022-05-21 20:15:35 +0530

    removed unused error statements

--- models/score.js
+++ models/score.js
@@ -2,17 +2,17 @@ const mongoose = require("mongoose");
 const ScoreSchema = new mongoose.Schema({
   contestName: {
     type: String,
-    required: [true, "must provide a existing contest name"],
+    required: [true],
     trim: true,
     maxlength: [200, "contest name can't be more than 200 chars long"]
   },
   userName: {
     type: String,
-    required: [true, "must provide a existing user name"],
+    required: [true],
     trim: true,
     maxlength: [200, "user name can't be more than 200 chars long"]
   },
-  usescoreName: {
+  userScore: {
     type: Number,
     default:0
   },
