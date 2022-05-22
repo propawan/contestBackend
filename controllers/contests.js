8a165135ffd6f9b07f002385462907216d7b7782
Vamsi_Krishna_027 <vamsi@oustlabs.com>
2022-05-22 20:36:17 +0530

    Added get contest users and scores api

--- controllers/contests.js
+++ controllers/contests.js
@@ -1,7 +1,23 @@
 const Contest = require("../models/contest");
+const Score = require("../models/score");
 const createContest = async (req, res) => {
   const contest = await Contest.create(req.body);
   return res.status(201).json({ contest });
 };
 
-module.exports = { createContest };
+const getContestUsers = async (req, res) => {
+  const { id: reqContestId } = req.params;
+  const scores = await Score.find({ contestId: reqContestId });
+  if (scores == null || scores.length == 0) {
+    return res
+      .status(200)
+      .json({ message: `No users under this contest id:- ${reqContestId}` });
+  }
+  const userScores = [];
+  for (let i = 0; i < scores.length; i++) {
+    userScores.push({ "Name": scores[i].userName, "Score" : scores[i].userScore });
+  }
+  return res.status(200).json({ userScores });
+};
+
+module.exports = { createContest, getContestUsers };
--- routes/api/v1/contests.js
+++ routes/api/v1/contests.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { createContest } = require("../../../controllers/contests");
+const {
+  createContest,
+  getContestUsers,
+} = require("../../../controllers/contests");
 
 router.route("/").post(createContest);
+router.route("/:id").get(getContestUsers);
 
 module.exports = router;
