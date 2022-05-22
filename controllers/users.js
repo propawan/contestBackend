4f2ed34c4eb305a9f87f4bfbbfb316a78b9dfa24
Pawan Kumar <pawankumarpawankumar619@gmail.com>
2022-05-23 02:51:13 +0530

    Add User authentication functionality.

--- controllers/users.js
+++ controllers/users.js
@@ -1,7 +1,16 @@
+const bcrypt = require("bcryptjs");
+const BadRequest = require("../errors/bad-request");
 const { createCustomError } = require("../errors/custom-error");
+const UnauthenticatedError = require("../errors/unauthenticated");
 const User = require("../models/user");
+const jwt = require("jsonwebtoken");
 
 const createUser = async (req, res) => {
+  let password = req.body.password;
+  if (!password) {
+    throw createCustomError("Password is required", 400);
+  }
+  req.body.password = bcrypt.hashSync(password);
   const user = await User.create(req.body);
   return res.status(201).json({ user });
 };
@@ -15,4 +24,23 @@ const getUser = async (req, res) => {
   return res.status(200).json({ user });
 };
 
-module.exports = { createUser, getUser };
+const signIn = async (req, res) => {
+  const { userName, password } = req.body;
+  if (!userName || !password) {
+    throw createCustomError("Please provide email and password", 400);
+  }
+  const user = await User.findOne({ userName: userName });
+  if (!user) {
+    throw new BadRequest(`No user exist with userName : ${userName}`);
+  }
+  const isPasswordValid = bcrypt.compareSync(password, user.password);
+  if (!isPasswordValid) {
+    throw new UnauthenticatedError("Password is incorrect");
+  }
+  const token = jwt.sign({ id: user._id, userName }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+  res.status(200).json({ msg: "User signed in", token });
+};
+
+module.exports = { createUser, getUser, signIn };
--- routes/api/v1/users.js
+++ routes/api/v1/users.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { createUser, getUser } = require("../../../controllers/users");
+const { createUser, getUser, signIn } = require("../../../controllers/users");
 const router = express.Router();
 
 router.route("/").post(createUser);
+router.route("/signin").post(signIn);
 router.route("/:id").get(getUser);
 
 module.exports = router;
