5f3a5735f44ec9ccd3d53fb4353a076f5910b723
Tushar Agrawal <tushar.agarwal@unthinkable.co>
2022-05-22 23:44:20 +0530

    Add api for deleting and updating contest

--- controllers/contests.js
+++ controllers/contests.js
@@ -1,4 +1,5 @@
 const { createCustomError } = require("../errors/custom-error");
+const contest = require("../models/contest");
 const Contest = require("../models/contest");
 const Score = require("../models/score");
 
@@ -45,13 +46,65 @@ const getAllParticipants = async (req, res) => {
 const getOnGoingContest = async (req, res) => {
   const contests = await Contest.find({});
   let today = new Date();
+  let todayDate = new Date(
+    today.getFullYear(),
+    today.getMonth(),
+    today.getDate(),
+    0,
+    0,
+    0,
+    0
+  );
+
   console.log(today);
   console.log(today.getTime());
+
+  console.log(todayDate);
+  console.log(todayDate.getTime());
   let onGoingContest = contests.filter(function (cn, i) {
-    return cn.contestDateAndTime.getTime() === today.getTime();
+    return (
+      cn.contestDateAndTime.getTime() >= todayDate.getTime() &&
+      cn.contestDateAndTime.getTime() <= today.getTime()
+    );
   });
 
   return res.json({ onGoingContest });
+};
+
+const deleteContest = async (req, res) => {
+  const id = req.params.id;
+
+  let contest = await Contest.find({ _id: id });
+
+  if (contest == null || contest.length == 0) {
+    return res.status(404).json({ message: "Cannot find Contest" });
+  }
+
+  await Contest.deleteOne({ _id: id });
+
+  let score = await Score.find({ contestId: contest._id });
+
+  score.map(async function (v, i) {
+    //if (v.contestId == id) {
+    await Score.deleteOne(v._id);
+    // }
+  });
+
+  return res.status(200).json({ message: "Contest has been deleted" });
+};
+
+const updateContest = async (req, res) => {
+  const contest = req.body;
+  let updatedContest = await Contest.findByIdAndUpdate(contest._id, contest);
+
+  let score = await Score.find({ contestId: contest._id });
+
+  score.map(async function (v, i) {
+    v.contestName = contest.contestName;
+    await Score.findByIdAndUpdate(v._id, v);
+  });
+
+  return res.status(200).json({ updateContest });
 };
 
 module.exports = {
@@ -60,4 +113,6 @@ module.exports = {
   getAllContest,
   getAllParticipants,
   getOnGoingContest,
+  deleteContest,
+  updateContest,
 };
