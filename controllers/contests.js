e2d913ce98ecac62d7a70f12904fae1569aaa0b9
Pawan Kumar <pawankumarpawankumar619@gmail.com>
2022-05-26 19:58:29 +0530

    Add contest registration API.

--- controllers/contests.js
+++ controllers/contests.js
@@ -1,9 +1,31 @@
+const BadRequest = require("../errors/bad-request");
 const Contest = require("../models/contest");
 const createContest = async (req, res) => {
-  console.log("Inside Contest");
-  console.log(req.user);
   const contest = await Contest.create(req.body);
   return res.status(201).json({ contest });
 };
 
-module.exports = { createContest };
+const registerInContest = async (req, res) => {
+  const { id: userId } = req.user;
+  const { contestId } = req.params;
+  const contest = await Contest.findOne({ _id: contestId });
+  if (contest == null || userId == null) {
+    throw new BadRequest("Please provide correct contest id and userId.");
+  }
+  if (contest.users && contest.users.includes(userId)) {
+    throw new BadRequest("User already registered.");
+  }
+  contest.users.push(userId);
+  const updatedContest = await Contest.findOneAndUpdate(
+    { _id: contestId },
+    contest,
+    {
+      new: true,
+      runValidators: true,
+    }
+  );
+
+  return res.status(200).json({ updatedContest });
+};
+
+module.exports = { createContest, registerInContest };
--- routes/api/v1/contests.js
+++ routes/api/v1/contests.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const { createContest } = require("../../../controllers/contests");
+const {
+  createContest,
+  registerInContest,
+} = require("../../../controllers/contests");
 const authenticationMiddleware = require("../../../middlewares/auth");
 
 router.route("/").post(authenticationMiddleware, createContest);
+router
+  .route("/register/:contestId")
+  .post(authenticationMiddleware, registerInContest);
 
 module.exports = router;
